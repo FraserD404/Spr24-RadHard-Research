a9b603434e737197b4bf3569320644572c22e96f
Fraser Dougall <fdougall1@gmail.com>
2024-04-04 23:52:14 -0400

    Update radpicode.c

--- radpicode.c
+++ radpicode.c
@@ -1,15 +1,28 @@
+/*
+
+C Code for EEPROM Control for Research
+
+April 4, 2024
+
+Author: Fraser Dougall 
+Email: fdougall@purdue.edu
+
+*/
+
 #include <stdio.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <time.h>
+#include <stdbool.h>
 
-#define BANK_SELECT_1 11
-#define BANK_SELECT_2 13
+// Selector Pins
+#define BANK_SELECT_1 0
+#define BANK_SELECT_2 1
 
+// EEPROM Banking Information
 #define EEPROM_ADDRESS 0x50 // base EEPROM I2C address
-
 #define NUM_BANKS 3
 #define EEPROMS_PER_BANK 8
 
@@ -20,13 +33,12 @@
 int getEEPROMSize(int bank, int num) {
     int sizeKB;
 
-    // how many kilobytes 
     switch(bank){
         case 0: 
             if(num >= 0 && num <= 3) { 
                 sizeKB = 4;
-            } else { 
-                sizeKB = 512; 
+            } else {
+                sizeKB = 16; 
             }
 
             break;
@@ -39,7 +51,11 @@ int getEEPROMSize(int bank, int num) {
 
             break;
         case 2: 
-            sizeKB = 4; 
+            if(num >= 0 && num <= 3) { 
+                sizeKB = 4;
+            } else {
+                sizeKB = -1; 
+            } 
 
             break;
         default:
@@ -48,9 +64,12 @@ int getEEPROMSize(int bank, int num) {
     }
 
     // kilobytes -> bytes
-    return 1000 * size; 
+    return 1000 * sizeKB; 
 }
 
+/*
+Initialize GPIO Pins
+*/
 void initGPIO() {
     wiringPiSetup();
 
@@ -61,19 +80,23 @@ void initGPIO() {
 void selectBank(int bank) {
     switch (bank) {
         case 0:
+	
+	
             digitalWrite(BANK_SELECT_1, LOW);
             digitalWrite(BANK_SELECT_2, LOW);
 
             break;
         case 1:
+	    
             digitalWrite(BANK_SELECT_1, HIGH);
             digitalWrite(BANK_SELECT_2, LOW);
-
+	    
             break;
         case 2:
-            digitalWrite(BANK_SELECT_1, LOW);
+	    
+            digitalWrite(BANK_SELECT_1, HIGH);
             digitalWrite(BANK_SELECT_2, HIGH);
-
+	    
             break;
         default:
             printf("Invalid bank number\n");
@@ -82,25 +105,37 @@ void selectBank(int bank) {
     }
 }
 
-void init_EEPROMs() {
+/* 
+Initialize all EEPROMs to have 0xFF in all memory locations
+*/
+void initEEPROMs() {
     for (int bank = 0; bank < NUM_BANKS; bank++) {
         selectBank(bank);
 
         for (int eeprom = 0; eeprom < EEPROMS_PER_BANK; eeprom++) {
             int eepromAddr = wiringPiI2CSetup(EEPROM_ADDRESS + eeprom);
 
-            if (eepromStatus == -1) {
+	    //if(bank == 2 && eeprom > 3) { 
+	//	    break;
+	  //  }
+
+            if (eepromAddr < 0) {
                 printf("Failed to initialize EEPROM %d in bank %d\n", eeprom, bank);
             } else {
-                for (int addr = 0; addr < getEEPROMSize; addr++) {
-                    if (wiringPiI2CWriteReg8(eepromAddr, addr, 0xFF) == -1) {
-                        printf("Failed to write to EEPROM %d in bank %d\n", eeprom, bank);
+		        int sizeBytes = getEEPROMSize(bank, eeprom);
+		        bool init = true;
 
+                for (int num = 0; num < sizeBytes; num++) {
+                    if (wiringPiI2CWrite(eepromAddr, 0xFF) == -1) {
+                        printf("Failed to write to EEPROM %d in bank %d\n", eeprom, bank);
+                        init = false;
                         break;
-                    }
+                    } 		
                 }
 
-                printf("Initialized EEPROM %d in bank %d\n", eeprom, bank);
+                if(init) {
+                    printf("Initialized EEPROM %d in bank %d\n", eeprom, bank);
+                }
 
                 close(eepromAddr);
             }
@@ -108,16 +143,17 @@ void init_EEPROMs() {
     }
 }
 
-void logger() {
-
+void logger(time_t startTime) {
     char filename[50];
 
     time_t t = time(NULL);
     struct tm tm = *localtime(&t);
 
-    sprintf(filename, "failures_%d-%02d-%02d_%02d-%02d-%02d.csv",
-            tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,
-            tm.tm_hour, tm.tm_min, tm.tm_sec);
+    //time_t startTime = time(NULL); 
+    time_t currTime = 0;
+    int elapsedTime = 0; 
+
+    sprintf(filename, "failures_%d-%02d-%02d_%02d-%02d-%02d.csv",tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday,   tm.tm_hour, tm.tm_min, tm.tm_sec);
 
     FILE *csv_file = fopen(filename, "a");
 
@@ -134,12 +170,17 @@ void logger() {
 
             if (eepromAddr == -1) {
                 printf("Failed to initialize EEPROM %d in bank %d\n", eeprom, bank);
-
-                fprintf("Bank %d, EEPROM%d failed to respond.\n", bank, eeprom);
+                fprintf("Bank %d, EEPROM%d, Failures: -1\n", bank, eeprom);
             } else {
                 int failures = 0;
+		int size = getEEPROMSize(bank, eeprom);
 
-                for (int addr = 0; addr < getEEPROMSize(bank, eeprom); addr++) {
+                // 512 done in 64k blocks otherwise takes too long
+                if(size/1000 == 512) {
+                    size = 64000;
+                }
+
+                for (int num = 0; num < size; num++) {
                     // Read the next byte in the eeprom
                     uint8_t data = wiringPiI2CRead(eepromAddr);
 
@@ -149,10 +190,13 @@ void logger() {
                     }
                 }
 
-                fprintf(csv_file, "Bank %d, EEPROM %d, Failures: %d\n", bank, eeprom, failures);
-                printf("Bank %d, EEPROM %d, Failures: %d\n", bank, eeprom, failures);
+                currTime = time(NULL); 
+                elapsedTime = (int)difftime(currTime, startTime); 
 
-                close(eeprom_fd);
+                fprintf(csv_file, "Elapsed Time %d, Bank %d, EEPROM %d, Failures %d\n", elapsedTime, bank, eeprom, failures);
+                printf("Elapsed Time %d, Bank %d, EEPROM %d, Failures: %d\n", elapsedTime, bank, eeprom, failures);
+
+                close(eepromAddr);
             }
         }
     }
@@ -161,12 +205,28 @@ void logger() {
 }
 
 int main() {
-    init_GPIO();
-    init_EEPROMs();
+    initGPIO();
 
+    printf("In1t EEPROMS?");
+
+    int choice;
+
+    scanf("%d", &choice);
+
+    time_t ctime = time(NULL); 
+
+    printf("%d", choice);
+
+    if(choice == 1) {
+        initEEPROMs();  
+        printf("Initialization took %d seconds!\n", (int)difftime(time(NULL), ctime)); 
+    }
+
+    // Continuously log data
     while (1) {
-        logger();
-        sleep(5); // Read and log every 5 seconds
+        logger(ctime);
+
+        //sleep(5); // Read and log every 5 seconds
     }
 
     return 0;
